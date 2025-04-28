8417cfc2bb505435a90efee63531490ee4d95159
Luke Barrier <lukebbarrier06@outlook.com>
2025-04-28 01:48:44 -0400

    Update

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # create-l3-app
 
-create-l3-app is a CLI tool designed to scaffold full-stack, type-safe web applications using modern frameworks and technologies like Next.js, Bun, Hono, tRPC, and SQLite. Tailored for intermediate React developers, this tool accelerates development by providing a fast, minimal-bloat setup that streamlines the process of building scalable applications.
+create-l3-app is a CLI tool designed to scaffold full-stack, type-safe web applications using modern frameworks and technologies like Next.js, Postgres, and Prisma ORM. Tailored for intermediate React developers, this tool accelerates development by providing a fast, minimal-bloat setup that streamlines the process of building scalable applications.
+
+[Read the docs](https://www.lukebarrier.info/l3/docs)
 
 ## Features
 
-- 🚀 Type-safe APIs with tRPC: Create robust remote procedures without the overhead of REST APIs.
 - ⚙️ Full-stack development: Seamless integration with Next.js for building full-stack applications.
-- ⚡ High performance: Powered by Bun for ultra-fast execution and minimal dependencies.
+- ⚡ High performance: Built on top of Next.js and Postgres, with optimized performance and caching.
+- 🔥 Type-safe: Built on top of Prisma ORM, ensuring type-safety and data validation.
+- 💪 Fast development: Built on top of Next.js, providing a fast and efficient development experience.
 - 🛡️ Production-ready: Easy integration with popular tools and services for deployment.
 
 ## Usage
@@ -14,3 +17,5 @@ create-l3-app is a CLI tool designed to scaffold full-stack, type-safe web appli
 ```bash
 npx create-l3-app@latest
 ```
+
+[Get started](https://www.lukebarrier.info/l3/docs)
--- bin.js
+++ bin.js
@@ -1,142 +1,170 @@
 #!/usr/bin/env node
 
-const readline = require('readline');
-const { spawn, exec } = require('child_process');
-const fs = require('fs');
+const readline = require("readline");
+const { spawn, exec } = require("child_process");
+const fs = require("fs");
+
+import { execSync } from "node:child_process";
+
+function hasYarn() {
+  try {
+    execSync("yarn --version", { stdio: "ignore" });
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+const useYarn = hasYarn();
+const PM = useYarn ? "yarn" : "npm";
+
+const usePM = (command) => {
+  return "${PM} ${command}";
+};
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 function getProjectName() {
-    return new Promise(resolve => {
-        rl.question('Project name: ', name => {
-            resolve(name);
-        });
+  return new Promise((resolve) => {
+    rl.question("Project name: ", (name) => {
+      resolve(name);
     });
+  });
 }
 
 function getWebPort() {
-    return new Promise(resolve => {
-        rl.question('Web server port: ', port => {
-            resolve(+port);
-        });
+  return new Promise((resolve) => {
+    rl.question("Port: ", (port) => {
+      resolve(+port);
     });
+  });
 }
 
-function getAPIPort() {
-    return new Promise(resolve => {
-        rl.question('API server port: ', port => {
-            resolve(+port);
-        });
-    });
-}
-
-let PROJECT_NAME = '';
-let WEB_PORT = 5170;
-let API_PORT = 3001;
+let PROJECT_NAME = "my-app";
+let WEB_PORT = 8080;
 
 function createProject() {
-    return new Promise(resolve => {
-        const process = spawn('git', ['clone', 'https://github.com/lukeb06/create-l3-app', `./${PROJECT_NAME}`, '--depth', '1']);
+  return new Promise((resolve) => {
+    const process = spawn("git", [
+      "clone",
+      "https://github.com/lukeb06/create-l3-app",
+      `./${PROJECT_NAME}`,
+      "--depth",
+      "1",
+    ]);
 
-        process.on('close', code => {
-            resolve(code);
-        });
+    process.on("close", (code) => {
+      resolve(code);
     });
+  });
 }
 
 function runCommand(command) {
-    return new Promise(resolve => {
-        exec(`cd ${PROJECT_NAME} && ${command}`, () => {
-            resolve();
-        });
+  return new Promise((resolve) => {
+    exec(`cd ${PROJECT_NAME} && ${command}`, () => {
+      resolve();
     });
+  });
 }
 
 async function removeDefaultGit() {
-    return await runCommand('rm -rf .git && rm README.md');
+  return await runCommand("rm -rf .git && rm README.md");
 }
 
 async function createNewGit() {
-    return await runCommand('git init');
+  return await runCommand("git init");
 }
 
 async function installDeps() {
-    return await runCommand('bun install');
+  return await runCommand(usePM("install"));
 }
 
 async function initialCommit() {
-    return await runCommand('git add . && git commit -m "Initialized with create-l3-app"');
+  return await runCommand(
+    'git add . && git commit -m "Initialized with create-l3-app"',
+  );
 }
 
 function replaceInFile(file, search, replace) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(`./${PROJECT_NAME}/${file}`, 'utf8', (err, data) => {
-            if (err) return reject(err);
+  return new Promise((resolve, reject) => {
+    fs.readFile(`./${PROJECT_NAME}/${file}`, "utf8", (err, data) => {
+      if (err) return reject(err);
 
-            const newContent = data.replaceAll(search, replace);
+      const newContent = data.replaceAll(search, replace);
 
-            fs.writeFile(`./${PROJECT_NAME}/${file}`, newContent, 'utf8', err => {
-                if (err) return reject(err);
-                resolve();
-            });
-        });
+      fs.writeFile(`./${PROJECT_NAME}/${file}`, newContent, "utf8", (err) => {
+        if (err) return reject(err);
+        resolve();
+      });
     });
+  });
 }
 
 function createFile(file, content) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(`./${PROJECT_NAME}/${file}`, content, 'utf8', err => {
-            if (err) return reject(err);
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    fs.writeFile(`./${PROJECT_NAME}/${file}`, content, "utf8", (err) => {
+      if (err) return reject(err);
+      resolve();
     });
+  });
+}
+
+function cloneFile(file, newName) {
+  return new Promise((resolve, reject) => {
+    fs.copyFile(
+      `./${PROJECT_NAME}/${file}`,
+      `./${PROJECT_NAME}/${newName}`,
+      (err) => {
+        if (err) return reject(err);
+        resolve();
+      },
+    );
+  });
 }
 
 async function rewritePackageJSON() {
-    await replaceInFile('package.json', 'create-l3-app', PROJECT_NAME);
-    await replaceInFile('package.json', '-p 5170', `-p ${WEB_PORT}`)
+  await replaceInFile("package.json", "create-l3-app", PROJECT_NAME);
 }
 
-async function rewriteServerPort() {
-    await createFile('.env', `PORT=${API_PORT}`);
-    await replaceInFile('src/lib/api.ts', '3001', API_PORT);
-    await replaceInFile('src/hooks/use-api.tsx', '3001', API_PORT);
+async function rewritePort() {
+  await cloneFile(".env.example", ".env");
+  await replaceInFile(".env", "PORT=8080", `PORT=${WEB_PORT}`);
 }
 
 async function createREADME() {
-    await createFile('README.md', `# ${PROJECT_NAME}\n\nCreated with [create-l3-app](https://github.com/lukeb06/create-l3-app)`);
+  await createFile(
+    "README.md",
+    `# ${PROJECT_NAME}\n\nCreated with [create-l3-app](https://github.com/lukeb06/create-l3-app)`,
+  );
 }
 
 async function main() {
-    const projectName = await getProjectName();
-    const webPort = await getWebPort();
-    const apiPort = await getAPIPort();
+  const projectName = await getProjectName();
+  const webPort = await getWebPort();
 
-    PROJECT_NAME = projectName;
-    WEB_PORT = webPort;
-    API_PORT = apiPort;
+  PROJECT_NAME = projectName;
+  WEB_PORT = webPort;
 
-    console.log('Creating project...');
-    await createProject();
-    await removeDefaultGit();
-    await createNewGit();
+  console.log("Creating project...");
+  await createProject();
+  await removeDefaultGit();
+  await createNewGit();
 
-    console.log('Installing dependencies...');
-    await installDeps();
+  console.log("Installing dependencies...");
+  await installDeps();
 
-    await rewritePackageJSON();
-    await rewriteServerPort();
-    await createREADME();
+  await rewritePackageJSON();
+  await rewritePort();
+  await createREADME();
 
-    await initialCommit();
+  await initialCommit();
 
-    console.log('Project created!');
+  console.log("Project created!");
 
-    process.exit(0);
+  process.exit(0);
 }
 
 main();
-
